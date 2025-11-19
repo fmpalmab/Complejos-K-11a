cd007d785bd71645a98dbbf99323a90c67a6c815
@AkariBocchi <agustin.salgado@ug.uchile.cl>
2025-11-19 14:12:17 -0300

    Guardando mis cambios locales antes de sincronizar

--- models.py
+++ models.py
@@ -224,7 +224,6 @@ class CRNN_DETECTAR_LOCALIZAR(nn.Module):
         # Clasificador
         logits = self.classifier(x) # -> (B, num_classes, 500)
 
-<<<<<<< HEAD
         return logits
     
 
@@ -385,6 +384,3 @@ class SEED_LOCALIZAR(nn.Module):
 
         return logits
     
-=======
-        return logits
->>>>>>> 74dba8e9fc90736992cffea0d9125458a217168d
--- utils.py
+++ utils.py
@@ -368,7 +368,6 @@ def generate_metrics_report(model, dataloader, device, save_path):
         print(df_report)
         print("-------------------------------------------\n")
     except Exception as e:
-<<<<<<< HEAD
         print(f"Error al guardar el reporte CSV: {e}")
 
 
@@ -735,6 +734,3 @@ def plot_event_confusion_matrix_with_std(tp_mean, tp_std, fp_mean, fp_std, fn_me
         print(f"Matriz de confusión (Media ± DE) guardada en: {save_path}")
     except Exception as e:
         print(f"Error al guardar la matriz de confusión: {e}")
-=======
-        print(f"Error al guardar el reporte CSV: {e}")
->>>>>>> 74dba8e9fc90736992cffea0d9125458a217168d
